33420a9f8c83b6b03d65e175c695d6055c67efeb
Crystal Ly <crystal.m.ly@gmail.com>
2019-12-28 20:43:49 -0600

    moved scripts around to try to fix issue where api is working on local but not on website.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //api keys for maps, weather, gov data, etc. are in the config.js. This is for security reasons so API keys don't get stolen.
 //WHEN YOU ADD AN API KEY, ADD IT TO CONFIG.JS. 
 
-//var Gkey = config.myGKey;
+var Gkey = config.myGKey;
 
 var WeatherKey = config.myWKey;
 
