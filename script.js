270edbe6009f04de26938ed2cda02c99b2885dd8
Crystal Ly <crystal.m.ly@gmail.com>
2020-01-03 13:22:02 -0600

    removed google maps and APIs; not needed. Removed on-click function and fixed document.ready to encompass entire script.js

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-//api keys for maps, weather, gov data, etc. are in the config.js. This is for security reasons so API keys don't get stolen.. 
-var mapsKey = "AIzaSyCY_qAq37SMtQG_l9NvDHiXLr5A5FlhlZ0";
-
-//this key will only work on local machines. Must have another variable with a Maps API key that is in this script.
-
-var gKey = "";
-
-try {
-    gKey = config.myGKey;
-} catch (error) {
-    // no operation; keeps going
-}
-
 //this key will work on local and website.
 var weatherKey = 'd9370cf81c44dc3900380fcc44da127d';
 
@@ -32,22 +19,6 @@ $(document).ready(function () {
             if (!header.data('faded')) header.data('faded', 1).stop(true).fadeTo(400, 0.2);
         } else if (header.data('faded')) {
             header.data('faded', 0).stop(true).fadeTo(400, 1);
-        }
-    });
-
-    //when page loads, maps should be default Austin, TX, 50 mile range. THIS DOESN'T WORK YET.
-    //https://developers.google.com/maps/documentation/javascript/tutorial
-    document.addEventListener('DOMContentLoaded', function () {
-        var js_file = document.createElement('script');
-        js_file.type = 'text/javascript';
-        js_file.src = "https://maps.googleapis.com/maps/api/js?key=" + gKey + "&callback=initMap";
-        mapBox.html(js_file);
-
-        function initMap() {
-            mapBox = new google.maps.Map(mapBox), {
-                center: { lat: -34.397, lng: 150.644 },
-                zoom: 8
-            };
         }
     });
 
@@ -141,17 +112,6 @@ $(document).ready(function () {
 
     //when you click on submit, we will take note of city/state || zip, and radius. THIS DOESN'T WORK YET
     submitButton.click(function () {
-        //pull data Google Maps.
-
-
-
-
-        //using use the location from Google maps (radius) to find any public land from the Govt API. 
-        /* 
-        https://catalog.data.gov/dataset/usgs-national-boundary-dataset-nbd-downloadable-data-collectionbc141
-        https://catalog.data.gov/dataset/trail-line-and-point-features-u-s-fish-and-wildlife-service
-     
-        */
 
         var userCurrentWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + (usCity[0].value || zipcodeInput[0].value) + ",us&units=imperial&appid=" + weatherKey;
 
@@ -231,14 +191,7 @@ $(document).ready(function () {
         });
     });
 
-});
-
-
-// Links to Forest Service and BLM appear when state is clicked
-
-
-
-$(document).ready(function () { //is it okay to delete this? We have 2 document ready functions. Unless this doesn't work without it?
+    // Links to Forest Service and BLM appear when state is clicked
 
     // COLORADO -- On Click that only works one time
     $('#CO').one('click', function () {
@@ -288,37 +241,35 @@ $(document).ready(function () { //is it okay to delete this? We have 2 document 
     if (aTag.length <= 1) {
         aTag.remove;
     }
-});
 
 
 
-// Links to Forest Service and BLM appear when state is clicked
+    // Links to Forest Service and BLM appear when state is clicked
 
 
 
-// script for clickable map
-$("path, circle").hover(function (e) {
-    $('#info-box').css('display', 'block');
-    $('#info-box').html($(this).data('info'));
-});
-
-$("path, circle").mouseleave(function (e) {
-    $('#info-box').css('display', 'none');
-});
-
-$(document).mousemove(function (e) {
-    $('#info-box').css('top', e.pageY - $('#info-box').height() - 30);
-    $('#info-box').css('left', e.pageX - ($('#info-box').width()) / 2);
-}).mouseover();
-
-
-
-
-var ios = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-if (ios) {
-    $('a').on('click touchend', function () {
-        var link = $(this).attr('href');
-        window.open(link, '_blank');
-        return false;
+    // script for clickable map
+    $("path, circle").hover(function (e) {
+        $('#info-box').css('display', 'block');
+        $('#info-box').html($(this).data('info'));
     });
-}
+
+    $("path, circle").mouseleave(function (e) {
+        $('#info-box').css('display', 'none');
+    });
+
+    $(document).mousemove(function (e) {
+        $('#info-box').css('top', e.pageY - $('#info-box').height() - 30);
+        $('#info-box').css('left', e.pageX - ($('#info-box').width()) / 2);
+    }).mouseover();
+
+    var ios = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+    if (ios) {
+        $('a').on('click touchend', function () {
+            var link = $(this).attr('href');
+            window.open(link, '_blank');
+            return false;
+        });
+    }
+
+});
