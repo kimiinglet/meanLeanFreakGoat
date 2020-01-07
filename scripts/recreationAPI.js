80e6b141f729a94a3c415b46ce4ead92ff37c3a8
JayDub21 <justin.wofford.21@gmail.com>
2020-01-07 14:39:10 -0600

    api - retrieve facilities within state, then retrieve facility ID's

--- scripts/recreationAPI.js
+++ scripts/recreationAPI.js
@@ -1,5 +1,5 @@
 var redirectHelp = "https://cors-ut-bootcamp.herokuapp.com/";
-var queryURL = redirectHelp + "ridb.recreation.gov/api/v1/activities?query=campinglimit=5&offset=0";
+var queryURL = redirectHelp + "ridb.recreation.gov/api/v1/facilities?limit=5&state=TX";
 
 $.ajax({
     url: queryURL,
@@ -8,6 +8,17 @@ $.ajax({
     },
     method: "GET"
 }).then(function (response) {
-    console.log("HELLO");
     console.log(response);
+    var facID = response.RECDATA[2].FacilityID
+    console.log(facID);
+
+    $.ajax({
+        url: redirectHelp + "ridb.recreation.gov/api/v1/facilities/" + facID + "/campsites?limit=5&offset=0",
+        headers: {
+            apikey: "5dc5ca61-c304-482b-bc02-1fcb1007f0b2"
+        },
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    })
 });
