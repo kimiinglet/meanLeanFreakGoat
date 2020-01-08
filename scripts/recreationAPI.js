1cab5d2b528616a433d65d4c1cb9d7f0b729269c
JayDub21 <justin.wofford.21@gmail.com>
2020-01-08 11:28:38 -0600

    added for loop to access Facility ID #'s

--- scripts/recreationAPI.js
+++ scripts/recreationAPI.js
@@ -1,6 +1,7 @@
+//CORS 
 var redirectHelp = "https://cors-ut-bootcamp.herokuapp.com/";
 // API Url for Facilities in TX
-var queryURL = redirectHelp + "ridb.recreation.gov/api/v1/facilities?limit=5&state=TX";
+var queryURL = redirectHelp + "ridb.recreation.gov/api/v1/facilities?limit=50&state=CO";
 
 var cSites = document.getElementById('cSites');
 
@@ -12,34 +13,34 @@ $.ajax({
     },
     method: "GET"
 }).then(function (response) {
-    console.log(response);
+    // console.log(response);
+    // console.log(response.RECDATA);
+    // console.log(response.RECDATA[0].FacilityID);
 
-    // Pulls the Facility ID #
-    var facID = response.RECDATA[2].FacilityID
-    // for (i=0; i<response.RECDATA.maxLength; i++) {
-
-    // }
-    console.log(facID);
-
-    // Uses Facility ID # to display all campsites within Facility
-    $.ajax({
-        url: redirectHelp + "ridb.recreation.gov/api/v1/facilities/" + facID + "/campsites?limit=5&offset=0",
-        headers: {
-            apikey: "5dc5ca61-c304-482b-bc02-1fcb1007f0b2"
-        },
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
+    // Loop thru results and grab the ALL the Facility ID #s
+    var recData = response.RECDATA;
+    for (var i = 0; i < recData.length; i++) {
+        // console.log(recData[i].FacilityID);
+        var facID = recData[i].FacilityID;
 
 
+        // Uses Facility ID # to display all campsites within Facility
+        $.ajax({
+            url: redirectHelp + "ridb.recreation.gov/api/v1/facilities/" + facID + "/campsites?limit=5&offset=0",
+            headers: {
+                apikey: "5dc5ca61-c304-482b-bc02-1fcb1007f0b2"
+            },
+            method: "GET"
+        }).then(function (response) {
+            // console.log(response.RECDATA);
+
+            if (response.RECDATA.length >= 1) {
+                console.log(response.RECDATA);
+                // var campGrounds = response.RECDATA;
+                // for (var i = 0; i < campGrounds.length; i++)
+            }
+        });
 
 
-
-        // if {
-        //     response.RECDATA[1].AttributeValue = true;
-        //     $('#TX').on('click', function () {
-        //         cSites.setAttribute(response.RECDATA[]);
-        //         cSites.innerText = "Texas BLM";
-
-    })
+    }
 })
